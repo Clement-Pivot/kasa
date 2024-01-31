87259f8fa6c0db5b24c63e6bd96c20a70641b5d0
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-01-31 17:22:34 +0100

    feat: navbar OK

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+import './index.scss'
+import { ReactComponent as Logo } from '../../assets/LOGO.svg'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+function Navbar() {
+  const location = useLocation()
+  const [pathname, setPathname] = useState('')
+
+  useEffect(() => {
+    setPathname(location.pathname)
+  }, [location])
+
+  return (
+    <nav className="nav">
+      <Logo className="nav__logo" />
+      <ul className="nav__links">
+        <li>
+          <Link to="/" className={pathname === '/' ? 'active' : ''}>
+            Accueil
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className={pathname === '/about' ? 'active' : ''}>
+            A Propos
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
