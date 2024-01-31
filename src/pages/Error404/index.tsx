137f3264335c21d0ef388bed4e6462aec825b43f
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-01-31 18:16:36 +0100

    refactor(error404): add class names

--- src/pages/Error404/index.tsx
+++ src/pages/Error404/index.tsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 function Error404() {
   return (
-    <section>
-      <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+    <section className="error404">
+      <h1 className="error404__header">404</h1>
+      <h2 className="error404__text">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className="error404__link">
+        Retourner sur la page d'accueil
+      </Link>
     </section>
   )
 }
