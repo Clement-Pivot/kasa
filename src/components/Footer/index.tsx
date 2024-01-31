39793d8f4649af8c31da2c8927fa2095feb14030
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-01-31 17:23:04 +0100

    feat: footer OK

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,13 @@
 import './index.scss'
+import { ReactComponent as Logo } from '../../assets/LOGO.svg'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Logo className="footer__logo" />
+      <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './main.scss'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
@@ -13,6 +14,7 @@ root.render(
         <Route path="/" element={<div>Test</div>} />
         <Route path="*" element={<div>Erreur 404</div>} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>,
 )
