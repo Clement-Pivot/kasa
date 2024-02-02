beefc0042953faf51cc0f843008e3022c9a2174a
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-02 18:13:19 +0100

    feat(homeCard): use props data

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -1,1 +1,13 @@
+import { AccessedLogement } from '../../utils/types'
 import './index.scss'
+
+function HomeCard({ logement }: AccessedLogement) {
+  return (
+    <article className="homeCard">
+      <p className="homeCard__title">{logement.title}</p>
+      <img src={logement.pictures[0]} className="homeCard__img" alt="" />
+    </article>
+  )
+}
+
+export default HomeCard
