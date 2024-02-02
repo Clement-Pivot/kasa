c6060e5839c8e145cf7e3a52e8c6251b5bd5085b
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-02 18:12:53 +0100

    feat(Home): call API and generate homeCards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,32 @@
 import './index.scss'
+import { useEffect, useState } from 'react'
 import Hero from '../../components/Hero'
 import HomeCard from '../../components/HomeCard'
 import picture from '../../assets/homeHero.png'
+import { Logement } from '../../utils/types'
 
 function Home() {
+  const [logementsList, setLogementsList] = useState([])
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/data.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Probleme acces API')
+        }
+      })
+      .then((data) => {
+        setLogementsList(data)
+        data.map((d: any) => console.log(d))
+      })
+  }, [])
   return (
     <section className="content">
       <Hero picture={picture} text="Chez vous, partout et ailleurs" />
+      {logementsList.map((logement: Logement) => (
+        <HomeCard key={logement.id} logement={logement} />
+      ))}
     </section>
   )
 }
