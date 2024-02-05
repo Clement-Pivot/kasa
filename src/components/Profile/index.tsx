f39eadfd19d2a391da6bbcb1cd5ae7f4328fa648
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-06 00:24:16 +0100

    feat: add profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,19 @@
+import './index.scss'
+import { Host } from '../../utils/types'
+
+export default function Profile({ user }: { user: Host }) {
+  return (
+    <div className="profile">
+      <div className="profile__names">
+        {user.name.split(' ')[0]}
+        <br />
+        {user.name.split(' ')[1]}
+      </div>
+      <img
+        src={user.picture}
+        alt={'Photo de profil de ' + user.name}
+        className="profile__picture"
+      />
+    </div>
+  )
+}
