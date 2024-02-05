1341f0870c2edad9d445378afbfc34322b3ddbd9
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-05 21:34:56 +0100

    feat(ficheLogement): add logic

--- src/pages/FicheLogement/index.tsx
+++ src/pages/FicheLogement/index.tsx
@@ -1,1 +1,15 @@
 import './index.scss'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useApi } from '../../utils/hooks'
+import { Logement, FicheLogementParams } from '../../utils/types'
+
+export default function FicheLogement() {
+  const { logementId } = useParams<FicheLogementParams>()
+  const [logement, setLogement] = useState<Logement>()
+  const response = useApi('http://127.0.0.1:3000/data.json')
+  useEffect(() => {
+    setLogement(response.find((item) => item.id === logementId))
+  }, [response, logementId])
+  return <section className="logementContent"></section>
+}
