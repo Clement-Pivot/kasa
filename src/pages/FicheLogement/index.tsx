04733b3ef1ba9b3361affed36c0fa0445a34b6f3
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-06 02:25:54 +0100

    feat(logement): call collapse with content

--- src/pages/FicheLogement/index.tsx
+++ src/pages/FicheLogement/index.tsx
@@ -1,6 +1,6 @@
 import './index.scss'
 import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { useApi } from '../../utils/hooks'
 import { Logement } from '../../utils/types'
 import Carousel from '../../components/Carousel'
@@ -12,11 +12,25 @@ import Collapse from '../../components/Collapse'
 export default function FicheLogement() {
   const { logementId } = useParams<string>()
   const [logement, setLogement] = useState<Logement>()
+  const [equipmentsCollapse, setEquipmentsCollapse] = useState<ReactNode>()
   const response = useApi('http://127.0.0.1:3000/data.json')
 
   useEffect(() => {
     setLogement(response.find((item) => item.id === logementId))
   }, [response, logementId])
+
+  useEffect(() => {
+    setEquipmentsCollapse(
+      logement?.equipments.reduce(
+        (result, equipment) => (
+          <>
+            {result} {result.props.children && <br />} {equipment}
+          </>
+        ),
+        <></>,
+      ),
+    )
+  }, [logement])
 
   return (
     <section className="content logement">
@@ -35,7 +49,8 @@ export default function FicheLogement() {
             <Profile user={logement.host} />
           </div>
           <div className="logement__collapses">
-            <Collapse title="Description" content={logement.equipements} />
+            <Collapse title="Description" content={logement.description} />
+            <Collapse title="Équipements" content={equipmentsCollapse} />
           </div>
         </>
       ) : (
