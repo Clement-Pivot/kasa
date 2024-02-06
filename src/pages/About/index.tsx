7e4b0bb15386c3c05eade3efe3b4b6961b21d713
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-06 22:47:23 +0100

    refactor: useApi and check type

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,21 +1,27 @@
 import Collapse from 'components/Collapse'
 import './index.scss'
 import 'components/Collapse'
-import { useAbout } from 'utils/hooks'
+import { useApi } from 'utils/hooks'
 import { useEffect, useState } from 'react'
 import { CollapseInfo } from 'utils/types'
 
 export default function About() {
-  const collapseData = useAbout('http://localhost:3000/about.json')
-  const [collapseInfo, setCollapseInfo] = useState<Array<CollapseInfo>>([])
+  const collapseData = useApi('http://localhost:3000/about.json')
+  const [collapseInfo, setCollapseInfo] = useState<CollapseInfo[]>([])
 
   useEffect(() => {
     setCollapseInfo(
-      collapseData.map((item) => ({
-        id: item.id,
-        title: item.title,
-        content: <>{item.content}</>,
-      })),
+      collapseData.map((item) => {
+        if ((item as CollapseInfo).content !== undefined) {
+          return {
+            id: item.id,
+            title: item.title,
+            content: <>{(item as CollapseInfo).content}</>,
+          }
+        } else {
+          throw new Error('API data KO.')
+        }
+      }),
     )
   }, [collapseData])
 
