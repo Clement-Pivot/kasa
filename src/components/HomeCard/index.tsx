9e8dc46f85e0404c9b01f408596e9ca139142f7b
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-07 01:08:20 +0100

    feat(home): add grid layout

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -4,10 +4,17 @@ import './index.scss'
 function HomeCard({ logement }: { logement: Logement }) {
   return (
     <article className="homeCard">
-      <a className="homeCard__gradient" href={'/logement/' + logement.id}>
-        <p className="homeCard__title">{logement.title}</p>
-        <img src={logement.pictures[0]} className="homeCard__img" alt="" />
-      </a>
+      <a
+        className="homeCard__gradient"
+        href={'/logement/' + logement.id}
+        title={'Lien vers un ' + logement.title}
+      />
+      <p className="homeCard__title">{logement.title}</p>
+      <img
+        src={logement.pictures[0]}
+        className="homeCard__img"
+        alt={"Photo d'un " + logement.title}
+      />
     </article>
   )
 }
