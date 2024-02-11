0c369157b2d8b4e62eac605f9db06ade6e8cf8b8
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-11 20:55:42 +0100

    feat(Tag): shrink text when too big for parent ✨

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react'
 import './index.scss'
 
 type Props = { text: string }
 
 export default function Tag({ text }: Props): JSX.Element {
-  return <div className="tag">{text}</div>
+  const maxPhoneWidth: number = 768
+
+  useEffect(() => {
+    const parent = document.querySelector('.tag') as HTMLDivElement
+    const content = document.querySelector('.tag__content') as HTMLDivElement
+    if (document.body.getBoundingClientRect().width < maxPhoneWidth) {
+      content.style.fontSize = '10px'
+    } else {
+      content.style.fontSize = '14px'
+    }
+    while (
+      content.getBoundingClientRect().height >
+      parent.getBoundingClientRect().height
+    ) {
+      content.style.fontSize = `${parseInt(content.style.fontSize) - 1}px`
+    }
+  })
+
+  return (
+    <div className="tag">
+      <div className="tag__content">{text}</div>
+    </div>
+  )
 }
