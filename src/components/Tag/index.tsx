5e0282dce4e763a670bb45eab1b2c8e71ef95349
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-12 00:53:24 +0100

    fix(Tag): update font size when window resize 🐛

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -7,19 +7,24 @@ export default function Tag({ text }: Props): JSX.Element {
   const maxPhoneWidth: number = 768
 
   useEffect(() => {
-    const parent = document.querySelector('.tag') as HTMLDivElement
-    const content = document.querySelector('.tag__content') as HTMLDivElement
-    if (document.body.getBoundingClientRect().width < maxPhoneWidth) {
-      content.style.fontSize = '10px'
-    } else {
-      content.style.fontSize = '14px'
+    const updateSize = () => {
+      const parent = document.querySelector('.tag') as HTMLDivElement
+      const content = document.querySelector('.tag__content') as HTMLDivElement
+      if (document.body.getBoundingClientRect().width < maxPhoneWidth) {
+        content.style.fontSize = '10px'
+      } else {
+        content.style.fontSize = '14px'
+      }
+      while (
+        content.getBoundingClientRect().height >
+        parent.getBoundingClientRect().height
+      ) {
+        content.style.fontSize = `${parseInt(content.style.fontSize) - 1}px`
+      }
     }
-    while (
-      content.getBoundingClientRect().height >
-      parent.getBoundingClientRect().height
-    ) {
-      content.style.fontSize = `${parseInt(content.style.fontSize) - 1}px`
-    }
+    updateSize()
+    window.addEventListener('resize', updateSize)
+    return () => window.removeEventListener('resize', updateSize)
   })
 
   return (
